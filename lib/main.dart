c769d7dd848bd8bdb1bd6823834a4d2cbd5c3886
poeticdev23 <poeticdev23@gmail.com>
2025-05-22 14:44:11 +0900

    Chore:java, minSdk 및 파워 컨트롤을 위한 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mdk_on_air/layout/default_layout.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Paperlogy',
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: DefaultLayout(),
     );
   }
 }
