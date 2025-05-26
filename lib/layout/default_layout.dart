cfcfd4543ab77c32c4de65d814e0555bb2c43351
poeticdev23 <poeticdev23@gmail.com>
2025-05-26 15:50:17 +0900

    Design: 자동 점검 중 테두리 그라데이션

--- lib/layout/default_layout.dart
+++ lib/layout/default_layout.dart
@@ -1,12 +1,10 @@
+import 'package:animated_gradient_box/animated_gradient_box.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mdk_on_air/componant/sensor_column.dart';
-import 'package:mdk_on_air/componant/splash_screen.dart';
-import 'package:mdk_on_air/const/color.dart';
 import 'package:mdk_on_air/const/studio_states.dart';
 import 'package:mdk_on_air/const/style.dart';
 import 'package:mdk_on_air/model/studio_state_model.dart';
-import 'package:mdk_on_air/util/initializer.dart';
 import 'package:mdk_on_air/util/state_manager.dart';
 
 class DefaultLayout extends ConsumerWidget {
@@ -16,6 +14,16 @@ class DefaultLayout extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     // StudioState currentState = STATE_CHECK;
     StudioState currentState = ref.watch(studioStateProvider);
+    List<Color> borderGradientColors = [Colors.transparent];
+
+    if (currentState == STATE_CHECK) {
+      borderGradientColors = [
+        Color(0xFF4FC3F7), // Sky Blue
+        Color(0xFF2979FF), // Strong Blue
+        Color(0xFF7C4DFF), // Purple Accent
+        Color(0xFF00B0FF), // Azure
+      ];
+    }
 
     final mWidth = MediaQuery.of(context).size.width;
     final mHeight = MediaQuery.of(context).size.height;
@@ -27,69 +35,74 @@ class DefaultLayout extends ConsumerWidget {
       backgroundColor: currentState.bgColor,
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Container(
-          constraints: BoxConstraints(maxWidth: mWidth, maxHeight: mHeight),
-          child: Row(
-            children: [
-              Expanded(
-                child: Center(
-                  child: Text(
-                    currentState.text,
-                    style: currentState.fontStyle.copyWith(
-                      color: currentState.fontColor,
+        child: GradientBox(
+          colors: borderGradientColors,
+          animationDuration: Duration(seconds: 3),
+          borderWidth: 16,
+          child: Container(
+            constraints: BoxConstraints(maxWidth: mWidth, maxHeight: mHeight),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      currentState.text,
+                      style: currentState.fontStyle.copyWith(
+                        color: currentState.fontColor,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
                   ),
                 ),
-              ),
-              VerticalDivider(
-                thickness: 2.0,
-                color: currentState.fontColor.withAlpha(80),
-              ),
-              SizedBox(
-                width: sensorBoxWidth,
-                height: mHeight,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          '현재 온도',
-                          style: SENSOR_FONT_STYLE.copyWith(
-                            fontSize: sensorFontSize * 0.4,
-                            color: currentState.fontColor,
-                          ),
-                        ),
-                        SensorText(
-                          provider: temperatureProvider,
-                          unitString: '℃',
-                          fontSize: sensorFontSize,
-                          fontColor: currentState.fontColor,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          '현재 습도',
-                          style: SENSOR_FONT_STYLE.copyWith(
-                            fontSize: sensorFontSize * 0.4,
-                            color: currentState.fontColor,
-                          ),
-                        ),
-                        SensorText(
-                          provider: humidityProvider,
-                          unitString: '%',
-                          fontSize: sensorFontSize,
-                          fontColor: currentState.fontColor,
-                        ),
-                      ],
-                    ),
-                  ],
+                VerticalDivider(
+                  thickness: 2.0,
+                  color: currentState.fontColor.withAlpha(80),
                 ),
-              ),
-            ],
+                SizedBox(
+                  width: sensorBoxWidth,
+                  height: mHeight,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            '현재 온도',
+                            style: SENSOR_FONT_STYLE.copyWith(
+                              fontSize: sensorFontSize * 0.4,
+                              color: currentState.fontColor,
+                            ),
+                          ),
+                          SensorText(
+                            provider: temperatureProvider,
+                            unitString: '℃',
+                            fontSize: sensorFontSize,
+                            fontColor: currentState.fontColor,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            '현재 습도',
+                            style: SENSOR_FONT_STYLE.copyWith(
+                              fontSize: sensorFontSize * 0.4,
+                              color: currentState.fontColor,
+                            ),
+                          ),
+                          SensorText(
+                            provider: humidityProvider,
+                            unitString: '%',
+                            fontSize: sensorFontSize,
+                            fontColor: currentState.fontColor,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
